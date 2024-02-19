984667368c8f8335fc9c781249d7fc5fa7196510
JotaCaps <149966521+JotaCaps@users.noreply.github.com>
2024-02-18 21:36:15 -0300

    Iniciei o projeto

---
+++ Models/Estoque.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controle_De_Estoque_Regatas_NBA.Models
+{
+    public class Estoque
+    {
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Controle_De_Estoque_Regatas_NBA.Models;
